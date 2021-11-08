e9259dbeaf0a50a2bc74892d70016985175f456c
Ayça ege Kayhan <aycaegekayhan@Aycas-MacBook-Air.local>
2021-11-07 21:52:06 -0500

    unarchivedfromfile function added

--- MovingManGame/MovingManGame/SKNode+Extensions.swift
+++ MovingManGame/MovingManGame/SKNode+Extensions.swift
@@ -9,4 +9,25 @@ import SpriteKit
 
 extension SKNode {
     
+    class func unarchiveFroFile(file: String) -> SKNode? {
+        
+        if let path = Bundle.main.path(forResource: file, ofType: "sks") {
+            let url = URL(fileURLWithPath: path)
+            do{
+                let sceneData = try Data(contentsOf: url, options: .mappedIfSafe) // if it is possible and safe the data will be mapped
+                let archiver = NSKeyedUnarchiver(forReadingWith: sceneData)
+                archiver.setClass(self.classForKeyedArchiver(), forClassName: "SKScene")
+                let scene = archiver.decodeObject(forKey: NSKeyedArchiveRootObjectKey) as! SKNode
+                archiver.finishDecoding()
+                return scene
+            } catch {
+                print(error.localizedDescription)
+                return nil
+            }
+        } else {
+            return nil
+        }
+        
+    }
+    
 }
