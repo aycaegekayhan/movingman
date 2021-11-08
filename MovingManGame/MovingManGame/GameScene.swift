4db9f185d63cc91a843eca68b6de607b4a148bb6
Ayça ege Kayhan <aycaegekayhan@Aycas-MacBook-Air.local>
2021-11-07 22:29:09 -0500

    adjust the tile map to the screen

--- MovingManGame/MovingManGame/GameScene.swift
+++ MovingManGame/MovingManGame/GameScene.swift
@@ -21,6 +21,14 @@ class GameScene: SKScene {
         if let levelNode = SKNode.unarchiveFroFile(file: level) {
             mapNode = levelNode
             addChild(mapNode) // to add map to game
+            loadTileMap() // scale is correct, tile map visible on the screen
+        }
+    }
+    
+    func loadTileMap() {
+        if let groundTiles = mapNode.childNode(withName: "Ground Tiles") as? SKTileMapNode{ // if get a value for ground tiles it will be sktilemapnode
+            tileMap = groundTiles
+            tileMap.scale(to: frame.size, width: false, multiplier: 1.0) //frame.size -> entire size of the sceene, adjust the size according to size
         }
     }
     
