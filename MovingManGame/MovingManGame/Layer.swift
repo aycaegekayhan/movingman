99dcc77594f1b56134c6334e6dcbbacac7566a2c
Ayça ege Kayhan <aycaegekayhan@Aycas-MacBook-Air.local>
2021-11-09 15:42:36 -0500

    layer class updated: add and multiply functions defined, functions added to update nodes

--- MovingManGame/MovingManGame/Layer.swift
+++ MovingManGame/MovingManGame/Layer.swift
@@ -7,6 +7,36 @@
 
 import SpriteKit
 
+public func * (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+public func + (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+public func += (left: inout CGPoint, right: CGPoint) {
+    left = left + right
+}
+
 class Laye: SKNode {
+    
+    var layerVelocity = CGPoint.zero
+    
+    func update(_ delta: TimeInterval) { // will call when work with layer
+        for child in children{ //all children in layer node
+            updateNodesGlobal(delta, childNode: child) // change the position of the respective child node by calculating offset
+        }
+    }
+    
+    func updateNodesGlobal(_ delta: TimeInterval, childNode: SKNode) {
+        let offset = layerVelocity * CGFloat(delta) // take the velocity of layer and multiply it with the time that has passed
+        childNode.position += offset //update the position of the child node
+        updateNodes(delta, childNode: childNode) // if necessary the nodes will be updated
+    }
+    
+    func updateNodes(_ delta: TimeInterval, childNode: SKNode) {
+        // will be overriden in subclasses
+    }
     
 }
