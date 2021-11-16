90281fd783a94abc5c19fa1b4d969903f832286d
Ayça ege Kayhan <aycaegekayhan@Aycas-MacBook-Air.local>
2021-11-15 22:26:37 -0500

    moving layer added, nodes updated

--- MovingManGame/MovingManGame/RepeatingLayer.swift
+++ MovingManGame/MovingManGame/RepeatingLayer.swift
@@ -8,4 +8,15 @@
 import SpriteKit
 
 class RepeatingLayer: Layer { //subclass of layer class
+    
+    override func updateNodes(_ delta: TimeInterval, childNode: SKNode) { //check if chil node move so much to left so that is not visible
+        if let node = childNode as? SKSpriteNode {
+            if node.position.x <= -(node.size.width) {
+                if node.name == "0" && self.childNode(withName: "1") != nil || node.name == "1" && self.childNode(withName: "0") != nil {
+                    node.position = CGPoint(x: node.position.x + node.size.width*2, y: node.position.y) //move it to right by doubling it's width.(for x position)
+                }
+            }
+        }
+    }
+    
 }
