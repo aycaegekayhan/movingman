a5eda9af57ceadd4246e562ab7dca713769da5c2
Ayça ege Kayhan <aycaegekayhan@Aycas-MacBook-Air.local>
2021-11-22 23:07:30 -0500

    object helper updated to check the names if name contains numbers it will considered as coins and will be treated as

--- MovingManGame/MovingManGame/ObjectHelper.swift
+++ MovingManGame/MovingManGame/ObjectHelper.swift
@@ -14,8 +14,19 @@ class ObjectHelper {
         case GameConstants.StringConstants.finishLineName, GameConstants.StringConstants.enemyName:
             PhysicsHelper.addPhysicsBody(to: sprite, with: name)
         default:
-            break
+            let component = name.components(separatedBy: NSCharacterSet.decimalDigits.inverted) // to take the numbers in the name
+            if let rows = Int(component[0]), let columns = Int(component[1]) {
+                calculateGridWith(rows: rows, columns: columns, parent: sprite)
+            }
         }
+    }
+    
+    static func calculateGridWith(rows: Int, columns:Int, parent: SKSpriteNode) {
+        
+    }
+    
+    static func addCoin(to parent: SKSpriteNode, at position: CGPoint, columns: Int) {
+        
     }
     
 }
